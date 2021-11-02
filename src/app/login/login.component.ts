8c6a7e5a27145234074e1fcbef1e5b857467c142
unknown <sibashishbiswas349@gmail.com>
2021-11-02 12:10:43 +0530

    Added logic for Signup and login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm!: FormGroup;
+  constructor(private formbuilder: FormBuilder, private _http:HttpClient, private _router:Router ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formbuilder.group({
+      email: [''],
+      password: ['']
+    });
   }
 
-}
+  logIn() {
+    //console.log(this.loginForm.value);
+    this._http.get<any>('http://localhost:3000/signup').subscribe(
+      (res) => {
+        const user= res.find((a:any)=>{
+          return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password;
+        });
+         if (user) {
+          alert(user.name + ' logged in successfully');
+          this._router.navigate(['/restaurent']);
+          this.loginForm.reset();
+         } else {
+          alert("Invalid credentials");
+         }
+        }, err=>{
+          console.log(err);
+        })
+      }
+    
+  }
+
+
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
-  constructor() { }
+  signupForm!: FormGroup
+  constructor(private formbuilder: FormBuilder, private _http:HttpClient, private _router:Router) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formbuilder.group({
+      name:[''],
+      email:[''],
+      mobile:[''],
+      password: ['']
+    })
+  }
+
+  signUp(){
+    this._http.post<any>('http://localhost:3000/signup',this.signupForm.value).subscribe(res=>{
+      console.log(res)
+      alert('Signup Successfully');
+      this.signupForm.reset();
+      this._router.navigate(['/login']);
+    }), (err: any)=>{
+      console.log(err);
+      alert('Signup Error');
+    }
   }
 
 }
